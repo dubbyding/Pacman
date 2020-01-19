2fc65217f1b5fd460e0f7638aa96fdd2df65be2d
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-01-19 12:54:34 +0545

    Moving Pacman done!

---
+++ GAME.C
@@ -0,0 +1,173 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<dos.h>
+int startAngle,endAngle,linexO,lineyO,linexT,lineyT;
+void angleCheck(int x,int y,int xinc,int yinc,int state){
+	//not moving state is 0
+	if(state==0){
+		startAngle = 225;
+		endAngle = 135;
+		linexO=x-8;
+		lineyO=y+6;
+		linexT=x-8;
+		lineyT=y-7;
+	}
+	if(state >= 1&&state<10){
+		if(xinc>0){
+			startAngle = 45;
+			endAngle = 315;
+			linexO=x+8;
+			lineyO=y-6;
+			linexT=x+8;
+			lineyT=y+7;
+		}else if(xinc<0){
+			startAngle = 225;
+			endAngle = 135;
+			linexO=x-8;
+			lineyO=y+6;
+			linexT=x-8;
+			lineyT=y-7;
+		}else if(yinc>0){
+			startAngle = 320;
+			endAngle = 230;
+			linexO=x-7;
+			lineyO=y+8;
+			linexT=x+7;
+			lineyT=y+8;
+		}else if(yinc<0){
+			startAngle = 135;
+			endAngle = 45;
+			linexO=x+7;
+			lineyO=y-8;
+			linexT=x-7;
+			lineyT=y-8;
+		}
+	}else if(state == 10){
+		startAngle = 0;
+		endAngle = 360;
+		if(xinc>0){
+			linexO = x+10;
+			lineyO = y;
+		}else if(xinc<0){
+			linexO = x-10;
+			lineyO = y;
+		}else if(yinc>0){
+			linexO = x;
+			lineyO = y+10;
+		}else if(yinc<0){
+			linexO = x;
+			lineyO = y-10;
+		}
+		linexT=linexO;
+		lineyT=lineyO;
+	}
+}
+void pacman(int x, int y,int xinc,int yinc,int state){
+	angleCheck(x,y,xinc,yinc,state);
+	//display
+	ellipse(x,y,startAngle,endAngle,10,10);
+	line(x,y,linexO,lineyO);
+	line(x,y,linexT,lineyT);
+}
+int main(){
+	int gd = DETECT, gm;
+	int x = 250, y = 200,xinc=0,yinc=0,xprev,yprev,state=0;
+	char inp=' ';
+	initgraph(&gd, &gm, "");
+	while(1){
+		cleardevice();
+		rectangle(10,10,630,450);
+		x += xinc;
+		y += yinc;
+		pacman(x,y,xinc,yinc,state);
+		if(inp!=' '){
+			if(state==10){
+				state = 1;
+			}else{
+				state++;
+			}
+		}
+		if(kbhit()){
+			inp = getch();
+			if(inp=='w'){
+				xinc = 0;
+				yinc = -1;
+			}else if(inp == 's'){
+				xinc = 0;
+				yinc = 1;
+			}else if(inp == 'a'){
+				xinc = -1;
+				yinc = 0;
+			}else if(inp == 'd'){
+				xinc = 1;
+				yinc = 0;
+			}else if(inp == 'q'){
+				break;
+			}else{
+				continue;
+			}
+		}
+		//
+
+		//
+		if(getpixel(x+11,y)>0){
+			if(xinc>0){
+				xinc=0;
+			}
+			if(getpixel(x,y-11)>0){
+				if(yinc < 0){
+					yinc = 0;
+				}
+			}else if(getpixel(x,y+11)>0){
+				if(yinc>0){
+					yinc = 0;
+				}
+			}
+		}else if(getpixel(x-11,y)>0){
+			if(xinc<0){
+				xinc = 0;
+			}
+			if(getpixel(x,y-11)>0){
+				if(yinc < 0){
+					yinc = 0;
+				}
+			}else if(getpixel(x,y+11)>0){
+				if(yinc>0){
+					yinc = 0;
+				}
+			}
+		}else if(getpixel(x,y-11)>0){
+
+			if(yinc < 0){
+				yinc = 0;
+			}
+			if(getpixel(x+11,y)>0){
+				if(xinc>0){
+					xinc=0;
+				}
+			}else if(getpixel(x-11,y)>0){
+				if(xinc<0){
+					xinc = 0;
+				}
+			}
+		}else if(getpixel(x,y+11)>0){
+			if(yinc>0){
+				yinc = 0;
+			}
+			if(getpixel(x+11,y)>0){
+				if(xinc>0){
+					xinc=0;
+				}
+			}else if(getpixel(x-11,y)>0){
+				if(xinc<0){
+					xinc = 0;
+				}
+			}
+		}
+		delay(40);
+	}
+	getch();
+	closegraph();
+	return 0;
+}
